fbee6a982fc4c9fab2fc24eee594bbe266db8274
dentuk <dentuk@free.fr>
2014-01-30 10:56:40 +0100

    Translated and documented utils

--- utils.erl
+++ utils.erl
@@ -4,68 +4,83 @@
 	 split_by/2, split_by_rev/2, split_by/3, split_by_rev/3,
 	 make_pairs/2, make_all_pairs/2]).
 
-% --- Génération de nombres aléatoires ---
+% --- Random data generation ---
 
-% Générer une liste de N nombres aléatoires compris entre 0 et 1.
+% Generate a list of N pseudo-random numbers uniformly distributed
+% between 0 and 1.
 generate_data(0) -> [];
 generate_data(N) -> [random:uniform() | generate_data(N-1)].
 
 
-% --- Séparation des N premiers éléments d'une liste ---
+% --- Splitting a list at a given position ---
 
-% Récupérer les N premiers éléments de la liste L d'une part, les
-% éléments restants d'autre part.
+% Get the N first elements of L on one hand, the other elements on the
+% other hand.
 split_at(L, N) ->
     {HRev, T} = split_at_rev(L, N),
     {lists:reverse(HRev), T}.
 
-% Récupérer les N premiers éléments (en sens inverse) de la liste
-% L d'une part, les éléments restants d'autre part.
+% Get the N first elements of L on one hand (in reverse order), the
+% other elements on the other hand.
 split_at_rev(L, N) -> split_at_rev(L, N, []).
 
-% Fonction auxiliare de split_at_rev/2.
+% Auxiliary function for split_at_rev/2.
 split_at_rev([], _, Stack) -> {Stack, []};
 split_at_rev(L, 0, Stack) -> {Stack, L};
 split_at_rev([H|T], N, Stack) -> split_at_rev(T, N - 1, [H|Stack]).
 
 
-% --- Séparation d'une liste selon un pivot ---
+% --- Partitionning a list over a pivot ---
 
-% Séparer la liste L en deux sous-listes : celle des éléments <= P,
-% et celle des éléments > P.
+% Partition L into two sublists : one containing elements <= P,
+% the other containing elements > P.
 split_by(L, P) ->
     {InfRev, SupRev} = split_by_rev(L, P),
     {lists:reverse(InfRev), lists:reverse(SupRev)}.
 
-% Séparer la liste L en deux sous-listes : celle des éléments <= P,
-% et celle des éléments > P.
+% Partition L into two sublists : one containing elements <= P,
+% the other containing elements > P. The elements will be in reversed
+% order.
 split_by_rev(L, P) -> split_by_rev(L, P, [], []).
 
+% Auxiliary function for split_by_rev/2.
 split_by_rev([], _, Inf, Sup) -> {Inf, Sup};
 split_by_rev([H|T], P, Inf, Sup) when H > P -> split_by_rev(T, P, Inf, [H|Sup]) ;
 split_by_rev([H|T], P, Inf, Sup) -> split_by_rev(T, P, [H|Inf], Sup).
 
+% Same as split_by(L1 ++ L2, P) but faster.
 split_by(L1, L2, P) ->
     {InfRev, SupRev} = split_by_rev(L1, L2, P),
     {lists:reverse(InfRev), lists:reverse(SupRev)}.
 
+% Same as split_by_rev(L1 ++ L2, P) but faster.
 split_by_rev(L1, L2, P) ->
     {InfRev, SupRev} = split_by_rev(L1, P, [], []),
     split_by_rev(L2, P, InfRev, SupRev).
 
+% --- Splitting a list into groups of paired elements ---
+
+% Make GroupSize pairs of objects on one hand, and get the remaining
+% elements on the other hand.
+% For instance:
+%     1> utils:make_pairs([1,2,3,4,5,6,7,8],2).
+%     {[{1,4},{2,3}],[5,6,7,8]}
 make_pairs(L, GroupSize) ->
-    %io:format("~p: make_pairs(~p,~p)~n",[self(),L, GroupSize]),
     {HRev, T} = split_at_rev(L, GroupSize),
     make_pairs(HRev, T, []).
 
+% Auxiliary function for make_pairs/2.
 make_pairs([], T, Pairs) -> {Pairs, T};
 make_pairs(T, [], Pairs) -> {Pairs, T};
 make_pairs([H0|T0], [H1|T1], Pairs) -> make_pairs(T0, T1, [{H0, H1}|Pairs]).
 
-
+% Apply make_pairs/2 until all list elements are paired, returning a
+% list of paired-elements lists.
+% Example:
+%     4> utils:make_all_pairs([1,2,3,4,5,6,7,8],2).
+%     [[{1,4},{2,3}],[{5,8},{6,7}]]
 make_all_pairs([], _) -> [];
 make_all_pairs(L, GroupSize) ->
-    %io:format("~p: make_all_pairs(~p,~p)~n",[self(),L, GroupSize]),
     {Pairs, T} = make_pairs(L, GroupSize),
     case Pairs of
 	[] -> [];
